f5825a145a574fe833b24c58e055291ceee4e163
spamuel <doylsamu@hotmail.com>
2025-03-11 12:23:59 -0400

    Cleaning

---
+++ src/main/java/net/xboxman/item/ModItems.java
@@ -0,0 +1,9 @@
+package net.xboxman.item;
+
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+import static net.xboxman.Andonium.MOD_ID;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MOD_ID);
+}
