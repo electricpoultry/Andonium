36deeef074de7000a68da3f54a17a51530b7c9f9
spamuel <doylsamu@hotmail.com>
2025-03-11 13:13:34 -0400

    Add Recipes

--- src/main/java/net/xboxman/datagen/ModRecipeProvider.java
+++ src/main/java/net/xboxman/datagen/ModRecipeProvider.java
@@ -4,9 +4,14 @@ import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.*;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.CampfireCookingRecipe;
+import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraft.world.level.ItemLike;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.neoforge.common.conditions.IConditionBuilder;
 import net.xboxman.item.ModItems;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public class ModRecipeProvider extends RecipeProvider implements IConditionBuilder {
@@ -16,11 +21,20 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
 
     @Override
     protected void buildRecipes(RecipeOutput recipeOutput) {
+        List<ItemLike> ANDONIUM_TO_DIAMOND_SHARDS = List.of(ModItems.ANDONIUM);
         ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, Items.DIAMOND, 1)
                 .requires(ModItems.DIAMOND_SHARD)
                 .requires(ModItems.DIAMOND_SHARD)
                 .requires(ModItems.DIAMOND_SHARD)
                 .requires(ModItems.DIAMOND_SHARD)
                 .unlockedBy("has_andonium", has(ModItems.ANDONIUM)).save(recipeOutput);
+
+        campfireCooking(recipeOutput, ANDONIUM_TO_DIAMOND_SHARDS, RecipeCategory.MISC, ModItems.DIAMOND_SHARD.get(), 2f, 620, "diamond_andonium");
+    }
+
+    protected static void campfireCooking(RecipeOutput recipeOutput, List<ItemLike> pIngredients, RecipeCategory pCategory, ItemLike pResult,
+                                      float pExperience, int pCookingTIme, String pGroup) {
+        oreCooking(recipeOutput, RecipeSerializer.CAMPFIRE_COOKING_RECIPE, CampfireCookingRecipe::new, pIngredients, pCategory, pResult,
+                pExperience, pCookingTIme, pGroup, "_from_smelting");
     }
 }
